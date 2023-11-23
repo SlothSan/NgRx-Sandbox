3619227d7e74ac2aefa64d86e17c19ec6409ecc2
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-11-23 13:43:16 +0000

    Created the form inputs and template updates.

--- src/app/feature/todo-list/components/task-search/task-search.component.ts
+++ src/app/feature/todo-list/components/task-search/task-search.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-search',
   templateUrl: './task-search.component.html',
-  styleUrls: ['./task-search.component.scss']
+  styleUrls: ['./task-search.component.scss'],
 })
-export class TaskSearchComponent {
+export class TaskSearchComponent implements OnInit, OnChanges {
+  private form?: FormGroup;
 
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(this.form);
+  }
+
+  buildForm() {
+    this.form = new FormGroup({
+      taskSearchInput: new FormControl('', Validators.minLength(3)),
+    });
+  }
+
+  get taskSearchInputFormControl(): FormControl {
+    return this.form?.get('taskSearchInput') as FormControl;
+  }
 }
